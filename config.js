c44899e57d5f8c277ba0efc9efb0013e4ed008d1
gitgirl88 <86039566+gitgirl88@users.noreply.github.com>
2022-05-02 13:59:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'ma j j',
+  title: 'notes',
   githubName: 'gitgirl88',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '马晶晶的博客，书写值得书写的东西',
+  SEO_description: 'bj的博客，书写值得书写的东西',
   SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
